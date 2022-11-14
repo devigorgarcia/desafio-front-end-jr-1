bcff984951ab33eff7f871658892d297103c8333
Igor Garcia <igorgg@gmail.com>
2022-11-14 18:11:42 -0300

    fix: condicional render on reward

--- src/components/Recive/index.tsx
+++ src/components/Recive/index.tsx
@@ -4,22 +4,42 @@ import "./index.css";
 
 export const Recive = () => {
   const { responseValue } = useContext(ValueContext);
-  
+
   return (
     <div className="reciveContainer">
       <div className="containerDiv">
         <h1>Você Receberá</h1>
         <p>
-          Amanha: <span>R$ {responseValue[1].toFixed(2)}</span>
+          Amanha:
+          {responseValue[1] ? (
+            <span> R$ {responseValue[1].toFixed(2)}</span>
+          ) : (
+            <span> R$ 0,00</span>
+          )}
         </p>
         <p>
-          Em 15 dias: <span>R$ {responseValue[15].toFixed(2)}</span>
+          Em 15 dias:{" "}
+          {responseValue[15] ? (
+            <span> R$ {responseValue[15].toFixed(2)}</span>
+          ) : (
+            <span> R$ 0,00</span>
+          )}
         </p>
         <p>
-          Em 30 dias: <span>R$ {responseValue[30].toFixed(2)}</span>
+          Em 30 dias:{" "}
+          {responseValue[30] ? (
+            <span> R$ {responseValue[30].toFixed(2)}</span>
+          ) : (
+            <span> R$ 0,00</span>
+          )}
         </p>
         <p>
-          Em 90 dias: <span>R$ {responseValue[90].toFixed(2)}</span>
+          Em 90 dias:{" "}
+          {responseValue[90] ? (
+            <span> R$ {responseValue[90].toFixed(2)}</span>
+          ) : (
+            <span> R$ 0,00</span>
+          )}
         </p>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ValuesProvider } from "./Contexts/value";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <ValuesProvider>
-      <App />
-    </ValuesProvider>
-  </React.StrictMode>
+  <ValuesProvider>
+    <App />
+  </ValuesProvider>
 );
